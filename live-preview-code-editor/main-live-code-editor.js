060851fb5e312319b777031bf1c2bc5054761430
thanhvo <thanhvo.uparis@gmail.com>
2025-07-24 16:53:34 +0100

    @live-code-editor: Done, but Paste cannot choose the position

--- live-preview-code-editor/main-live-code-editor.js
+++ live-preview-code-editor/main-live-code-editor.js
@@ -1,6 +1,7 @@
 const codeInput = document.querySelector("#editor-input");
 const previewFrame = document.querySelector("#preview-frame");
 const contextMenu = document.querySelector(".context-menu");
+let clipboardValue  = "";
 
 codeInput.addEventListener("input", function(event) {
     const editor = event.target.value;
@@ -19,6 +20,37 @@ codeInput.oncontextmenu = (event) => {
     });
 }
 
-document.onmousedown = () => {
+document.addEventListener("mousedown", (event) => {
+    if (event.button === 0 && !contextMenu.contains(event.target)) {
+        contextMenu.style.display = "none";
+    }
+})
+
+function handleCopy() {
+    codeInput.addEventListener("select", (event) => {
+            selectedText = event.target.value.substring(codeInput.selectionStart, codeInput.selectionEnd);
+    })
+}
+
+contextMenu.onclick = (event) => {
+    const context = event.target.closest("li");
+    if (context.classList.contains("btn-delete")) {
+        codeInput.value = "";
+        const codePreview = previewFrame.contentDocument || previewFrame.contentWindow.document;
+        codePreview.open();
+        codePreview.write("");
+    }
+    else if (context.classList.contains("btn-copy")) {
+        const selectedText = codeInput.value.substring(codeInput.selectionStart, codeInput.selectionEnd);
+        if (selectedText) {
+            clipboardValue = selectedText;
+        }
+    }
+    else if (context.classList.contains("btn-paste")) {
+        codeInput.value = codeInput.value + clipboardValue;
+        const codePreview = previewFrame.contentDocument || previewFrame.contentWindow.document;
+        codePreview.open();
+        codePreview.write(codeInput.value);
+    }
     contextMenu.style.display = "none";
 }
